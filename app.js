d9e1e5ca48e2124f8f4c1621da5b62865e1e06f3
Visalsvijayan <visalsvijayan@gmail.com>
2025-03-03 14:00:37 +0530

    view engine set up

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express=require('express');
 const app=express();
 const nodemon=require('nodemon')
+const path=require('path');
 const env=require('dotenv').config();
 const connectDB=require('./config/db');
+const userRouter=require('./routes/userRouter')
 connectDB();
+
+
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.set("view engine","ejs");
+app.set('views',[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')])
+app.use(express.static(path.join(__dirname,'public')))
+app.use('/',userRouter)
 
 
 
