448f6422493827d079028180961979c3deda3503
Visalsvijayan <visalsvijayan@gmail.com>
2025-05-20 15:16:49 +0530

    google signup change

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3006/auth/google/callback", // Ensure this matches Google Console
+      callbackURL: "http://dappermens.shop/auth/google/callback", // Ensure this matches Google Console
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
